4cb33f95028701b0d4f3a326846b5376001f53b0
Thiago Andrade <thiagoh@gmail.com>
2016-01-20 14:41:06 -0300

    bug fixing

--- bin/minify.js
+++ bin/minify.js
@@ -4,14 +4,33 @@
 
 var minifier = require('../lib/directory-minifier.js');
 
-var directory = process.argv[2], 
-	checksumFilepath = process.argv[3];
+var directory = process.argv[2],
+	checksumFilepath,
+	debug;
+
+for (var i = 0, leni = process.argv.length; i < leni; i++) {
+
+	var arg = process.argv[i];
+
+	if (arg === '-v' || arg === '--verbose') {
+		debug = true;
+
+	} else if (arg === '-c' || arg === '--checksum') {
+
+		if (i + 1 <= leni) {
+			checksumFilepath = process.argv[++i];
+		}
+	}
+}
 
 try {
 
-	minifier.minify(directory, checksumFilepath);
+	minifier.minify(directory, {
+		checksum: checksumFilepath,
+		debug: debug
+	});
 
 } catch (e) {
-	console.error(e)
+	console.error(e);
 	process.exit(1);
 }
